c538fc9e7fceb022e2114c18d660a2ebb9edd300
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-10-16 22:46:08 -0200

    potter: Use charging animation from stock

--- potter-vendor.mk
+++ potter-vendor.mk
@@ -20,6 +20,7 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/potter/proprietary/bin/akmd09912:system/bin/akmd09912 \
     vendor/motorola/potter/proprietary/bin/batt_health:system/bin/batt_health \
     vendor/motorola/potter/proprietary/bin/btnvtool:system/bin/btnvtool \
+    vendor/motorola/potter/proprietary/bin/charge_only_mode:system/bin/charge_only_mode \
     vendor/motorola/potter/proprietary/bin/cnd:system/bin/cnd \
     vendor/motorola/potter/proprietary/bin/dbvc_atvc_property_set:system/bin/dbvc_atvc_property_set \
     vendor/motorola/potter/proprietary/bin/hci_qcomm_init:system/bin/hci_qcomm_init \
